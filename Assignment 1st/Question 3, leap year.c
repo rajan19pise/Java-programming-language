718ed4f663e527cb75a0aafaecd2af6691cde976
rajan19pise <rajanpise2005@gmail.com>
2026-02-05 23:58:39 +0530

    Add files via upload

---
+++ Assignment 1st/Question 3, leap year.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int year = 2005;
+	if(year%4==0)
+	{
+		printf("The year is leap year");
+	}
+	else
+	{
+		printf("The year is not leap year");
+	}
+}
---
+++ Assignment 1st/Question 4, vowels or consonants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	int B = 'z';
+	if(B=='a'||B=='e'||B=='i'||B=='o'||B=='u')
+	{
+		printf("The character is vowel");
+	}
+	else
+	{
+		printf("The character is consonant");
+	}
+}
