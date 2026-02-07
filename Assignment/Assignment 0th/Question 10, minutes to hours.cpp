16ec947b79f27169d43d8d7c7e0f39126a5b3962
rajan19pise <rajanpise2005@gmail.com>
2026-02-07 20:13:42 +0530

    Delete Assignment directory

--- Assignment/Assignment 0th/Question 10, minutes to hours.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int minutes=123;
-	int hours=minutes/60;
-	int remaining=minutes%60;
-	printf("The total hours are %d and minutes are %d",hours, remaining);
-}
--- Assignment/Assignment 0th/Question 4, swapping numbers.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int no1 = 45;
-	int no2 = no1/10;
-	int no3 = no1%10;
-	printf ("result after swapping numbers are %d and %d", no2, no3 );
-}
